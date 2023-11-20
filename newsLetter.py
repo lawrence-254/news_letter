3c6086d1238782a9015f2b89d04b3ae27c1f88f3
lawrence-254 <lodhiambo26@gmail.com>
2023-11-20 15:59:16 +0300

    init

---
+++ newsLetter.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home/")
+def home():
+    return 'hello flask'
+
+
+@app.route("/about/")
+def about():
+    return 'about page'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
