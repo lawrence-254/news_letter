f7f0e275ca7165120da783f8ae46de0493704907
lawrence-254 <lodhiambo26@gmail.com>
2023-11-20 21:17:10 +0300

    layout and folders

--- newsLetter.py
+++ newsLetter.py
@@ -1,16 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+# dummy data
+posts = [
+    {
+        'author': 'peter griffin',
+        'title': 'peter copter',
+        'content': 'flying',
+        'date_posted': 'january 15, 2020'
+    },
+    {
+        'author': 'stew griffin',
+        'title': 'the duce',
+        'content': 'victory shall be mine',
+        'date_posted': 'february 14, 2020'
+    }
+]
+# end of dummy data
 
 
 @app.route("/")
 @app.route("/home/")
 def home():
-    return 'hello flask'
+    return render_template('home.html', posts=posts, title='Home')
 
 
 @app.route("/about/")
 def about():
-    return 'about page'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
