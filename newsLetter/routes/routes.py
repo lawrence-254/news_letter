2a90ace3266b56068dc03f942cd6eef1996d4f42
lawrence-254 <lodhiambo26@gmail.com>
2023-11-26 18:52:50 +0300

    0

--- newsLetter/routes/routes.py
+++ newsLetter/routes/routes.py
@@ -128,8 +128,15 @@ def post_update(post_id):
     if post.author != current_user:
         abort(403)
     form = PostForm()
-    form.title.data = post.title
-    form.content.data = post.content
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash(f'Post Update Success', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
     return render_template(
         'create_post.html',
         title='Update Your Post',
