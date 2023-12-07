12cce05bd9859d6f5d8dfaff3b02e9d23f9d450f
lawrence-254 <lodhiambo26@gmail.com>
2023-12-07 13:28:56 +0300

    config files

--- newsLetter/__init__.py
+++ newsLetter/__init__.py
@@ -3,18 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+
+from newsLetter.config import Config
+
 
 
 app = Flask(__name__)
-'''secret key'''
-app.config['SECRET_KEY'] = 'f43e0ff5e0ad8b9594af298c550b92ca'
 
+app.config.from_object(Config)
 '''sql db imports'''
 '''mysql config for deployment'''
-databaseLink = 'mysql+mysqlconnector://root@localhost/news_letter'
-app.config['SQLALCHEMY_DATABASE_URI'] = databaseLink
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 '''end of sql configs'''
 '''hashing'''
@@ -24,12 +23,6 @@ login_manager = LoginManager(app)
 login_manager.login_view ='users.login'
 login_manager.login_message_category ='danger'
 
-'''mail configs'''
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 
 mail = Mail(app)
---
+++ newsLetter/config.py
@@ -0,0 +1,19 @@
+import os
+class Config:
+    databaseLink = 'mysql+mysqlconnector://root@localhost/news_letter'
+    SQLALCHEMY_DATABASE_URI = databaseLink
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'f43e0ff5e0ad8b9594af298c550b92ca'
+    '''secret key'''
+    # SECRET_KEY = os.environ.get('SECRET_KE')
+
+    # '''db'''
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    '''mail configs'''
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
